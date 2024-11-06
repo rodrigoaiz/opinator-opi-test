96ac60005fd13248ed770fefd92417fa72705dc6
rodrigoaiz <rodrigoaiz@gmail.com>
2024-11-06 11:10:00 -0600

    funciona react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(),
+    react({
+      include: ['**/react/*'],
+    })
+  ]
 });
---
+++ src/components/GeneroSeleccionado.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { generos } from "@preguntas";
+
+const GeneroSeleccionado = () => {
+  const [selectedGenero, setSelectedGenero] = useState("");
+
+  const handleSelect = (genero: { nombre: string; img: string }) => {
+    console.log(`Selected genero: ${genero.nombre}`);
+    setSelectedGenero(genero.nombre);
+  };
+
+  return (
+    <section className="preguntauno">
+      <h2>¿Cuáles son los géneros que te gustan más?</h2>
+      <div className="generos">
+        {generos.map((genero) => (
+          <div
+            key={genero.nombre}
+            className={`generos__article ${selectedGenero === genero.nombre ? "selected" : ""}`}
+            onClick={() => handleSelect(genero)}
+          >
+            <img src={genero.img} alt={genero.nombre} />
+            <span>{genero.nombre}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default GeneroSeleccionado;
